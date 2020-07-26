150c9545e146c1e8d6750c1ae3599db9c4d81919
kakk22 <1029839308@qq.com>
2020-07-26 12:39:59 +0800

    eureka-server security

---
+++ eureka-security-server/src/main/java/com/cyf/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.cyf.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author by cyf
+ * @date 2020/7/26.
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
