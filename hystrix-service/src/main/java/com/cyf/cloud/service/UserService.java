3b6bdfbfc27a47c36c3a966f9865f23ab486a60c
kakk22 <1029839308@qq.com>
2020-07-28 21:45:46 +0800

    hystrixCommand 注解

--- hystrix-service/src/main/java/com/cyf/cloud/service/UserService.java
+++ hystrix-service/src/main/java/com/cyf/cloud/service/UserService.java
@@ -29,4 +29,22 @@ public class UserService {
         User defaultUser = new User(-1L, "defaultUser", "123456");
         return new CommonResult<>(defaultUser);
     }
+
+    /**
+     *
+     * fallbackMethod：指定服务降级处理方法；
+     * ignoreExceptions：忽略某些异常，不发生服务降级；
+     * commandKey：命令名称，用于区分不同的命令；
+     * groupKey：分组名称，Hystrix会根据不同的分组来统计命令的告警及仪表盘信息；
+     * threadPoolKey：线程池名称，用于划分线程池。
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "getDefaultUser",
+                    groupKey = "getUserGroup",
+                    commandKey = "getUserCommand",
+                    threadPoolKey = "getUserThreadPool")
+    public CommonResult getUserCommand(@PathVariable Long id) {
+        return restTemplate.getForObject(userServiceUrl+"/user/{1}",CommonResult.class,id);
+    }
 }
